9dfd5ef27e4e18dcbe64ba7df25826a6a11a7ddd
Denis280303 <kozlovskyidenus@gmail.com>
2022-08-25 21:06:05 +0300

    Message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.active_mailer.default_url_options = { host: "rocky-retreat-68446.herokuapp.com"}
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => 'apikey',
+    :password  => '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', # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
+    :domain => 'rocky-retreat-68446.herokuapp.com', # your domain to identify your server when connecting
+  }
 end
